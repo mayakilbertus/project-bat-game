6e4f3a3825d08e9c00146e4bea876937f72d3229
mayakilbertus <maya.kilbertus@yahoo.de>
2023-10-30 16:46:16 +0100

    creating Game Class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,100 @@
 class Game {
-  constructor;
+  constructor() {
+    this.batPlayer = new BatPlayer();
+    this.preyArr = [];
+    this.counterPrey = 0;
+    this.counterGameTime = 22;
+  }
+
+  start() {
+    this.whatever();
+    setInterval(() => {
+      this.counterPrey++;
+      this.preyElement = new Prey();
+      this.preyElement.createPrey(this.counterPrey);
+      this.preyArr.push(this.preyElement);
+    }, 3000);
+
+    setInterval(() => {
+      this.preyElement.removePrey(this.preyArr[0].prey.getAttribute("id"));
+      this.preyArr.shift();
+    }, 7000);
+
+    const timeElement = document.getElementById("time");
+    const timer = setInterval(() => {
+      if (this.counterGameTime == 24) {
+        this.counterGameTime = 0;
+        timeElement.innerHTML = `${this.counterGameTime}:00 h`;
+      } else {
+        this.counterGameTime++;
+        timeElement.innerHTML = `${this.counterGameTime}:00 h`;
+      }
+    }, 8000);
+  }
+
+  whatever() {
+    document.addEventListener("keydown", (event) => {
+      event.preventDefault;
+      switch (event.code) {
+        case "ArrowUp":
+          this.batPlayer.moveBatUp();
+          const preyDetectUp = this.batPlayer.checkForPrey(this.preyArr);
+          // this.preyDetectUp = this.batPlayer.checkForPrey(this.preyArr);
+          if (preyDetectUp) {
+            this.preyArr.splice(preyDetectUp[1], 1);
+            this.preyElement.removePrey(
+              preyDetectUp[0].prey.getAttribute("id")
+            );
+          }
+
+          // if (this.preyDetectUp) {
+          //   this.updatePrey(this.PreyDetectUp);
+          // }
+          break;
+        case "ArrowDown":
+          this.batPlayer.moveBatDown();
+          const preyDetectDown = this.batPlayer.checkForPrey(this.preyArr);
+          if (preyDetectDown) {
+            this.preyArr.splice(preyDetectDown[1], 1);
+            this.preyElement.removePrey(
+              preyDetectDown[0].prey.getAttribute("id")
+            );
+          }
+          break;
+        case "ArrowRight":
+          this.batPlayer.moveBatRight();
+          const preyDetectRight = this.batPlayer.checkForPrey(this.preyArr);
+          if (preyDetectRight) {
+            this.preyArr.splice(preyDetectRight[1], 1);
+            this.preyElement.removePrey(
+              preyDetectRight[0].prey.getAttribute("id")
+            );
+          }
+          break;
+        case "ArrowLeft":
+          this.batPlayer.moveBatLeft();
+          const preyDetectLeft = this.batPlayer.checkForPrey(this.preyArr);
+          if (preyDetectLeft) {
+            this.preyArr.splice(preyDetectLeft[1], 1);
+            this.preyElement.removePrey(
+              preyDetectLeft[0].prey.getAttribute("id")
+            );
+          }
+          break;
+        default:
+          break;
+      }
+    });
+  }
+  // updatePrey(preyDetect) {
+  //   this.preyArr.splice(preyDetect[1], 1);
+  //   this.preyElement.removePrey(preyDetect[0].prey.getAttribute("id"));
+  // }
+  end() {
+    if (this.batPlayer.showEnergyLevel() >= 80) {
+      window.alert("You survived the day!");
+    } else {
+      window.alert("You died!");
+    }
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,72 +1,35 @@
-let counterPrey = 0;
-const preyArr = [];
+// let counterPrey = 0;
+// const preyArr = [];
 
-setInterval(() => {
-  counterPrey++;
-  const prey = new Prey();
-  prey.createPrey(counterPrey);
-  preyArr.push(prey);
-}, 3000);
+// setInterval(() => {
+//   counterPrey++;
+//   const prey = new Prey();
+//   prey.createPrey(counterPrey);
+//   preyArr.push(prey);
+// }, 3000);
 
-setInterval(() => {
-  preyElement.removePrey(preyArr[0].prey.getAttribute("id"));
-  preyArr.shift();
-}, 7000);
+// setInterval(() => {
+//   preyElement.removePrey(preyArr[0].prey.getAttribute("id"));
+//   preyArr.shift();
+// }, 7000);
 
-let counterGameTime = 22;
-const timeElement = document.getElementById("time");
+// let counterGameTime = 22;
+// const timeElement = document.getElementById("time");
 
-const timer = setInterval(() => {
-  if (counterGameTime == 24) {
-    counterGameTime = 0;
-    timeElement.innerHTML = `${counterGameTime}:00 h`;
-  } else {
-    counterGameTime++;
-    timeElement.innerHTML = `${counterGameTime}:00 h`;
-  }
-}, 8000);
+// const timer = setInterval(() => {
+//   if (counterGameTime == 24) {
+//     counterGameTime = 0;
+//     timeElement.innerHTML = `${counterGameTime}:00 h`;
+//   } else {
+//     counterGameTime++;
+//     timeElement.innerHTML = `${counterGameTime}:00 h`;
+//   }
+// }, 8000);
+
+const game = new Game();
+
+game.start();
 
 setTimeout(() => {
-  clearInterval(timer);
-  if (batPlayer.showEnergyLevel() >= 80) {
-    window.alert("You survived the day!");
-  } else {
-    window.alert("You died!");
-  }
+  game.end();
 }, 70000);
-
-document.addEventListener("keydown", (event) => {
-  event.preventDefault;
-  if (event.code === "ArrowUp") {
-    batPlayer.moveBatUp();
-    const preyDetect = batPlayer.checkForPrey(preyArr);
-    if (preyDetect) {
-      preyArr.splice(preyDetect[1], 1);
-      preyElement.removePrey(preyDetect[0].prey.getAttribute("id"));
-    }
-  } else if (event.code === "ArrowDown") {
-    batPlayer.moveBatDown();
-    const preyDetect = batPlayer.checkForPrey(preyArr);
-
-    if (preyDetect) {
-      preyArr.splice(preyDetect[1], 1);
-      preyElement.removePrey(preyDetect[0].prey.getAttribute("id"));
-    }
-  } else if (event.code === "ArrowRight") {
-    batPlayer.moveBatRight();
-    const preyDetect = batPlayer.checkForPrey(preyArr);
-
-    if (preyDetect) {
-      preyArr.splice(preyDetect[1], 1);
-      preyElement.removePrey(preyDetect[0].prey.getAttribute("id"));
-    }
-  } else if (event.code === "ArrowLeft") {
-    batPlayer.moveBatLeft();
-    const preyDetect = batPlayer.checkForPrey(preyArr);
-
-    if (preyDetect) {
-      preyArr.splice(preyDetect[1], 1);
-      preyElement.removePrey(preyDetect[0].prey.getAttribute("id"));
-    }
-  }
-});
